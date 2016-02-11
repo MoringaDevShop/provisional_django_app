1a9728305d256d628dd915194142bdc151d729d7
Benson Mburu <nguruben@gmail.com>
2016-02-11 21:47:41 +0300

    models added

--- adlink_app/models.py
+++ adlink_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class customer (models.Model):
+	
+	firstname = models.CharField(max_length=100, unique=False)
+	lastname = models.CharField(max_length=100, unique=False)
+	email = models.EmailField(max_length=100, unique=True)
+	phone = models.CharField(max_length=100, unique=False)
+	company = models.CharField(max_length=100, unique=False)
+	profile_pic=models.FileField(upload_to='uploads/')
+	created_at =models.DateField(auto_now=True, auto_now_add=True)
+	updated_at = models.DateField(auto_now=True, auto_now_add=True)
+	def __unicode__(self):
+		return '%s'  %self.firstname 
+]
